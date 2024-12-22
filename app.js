dcd1d58ffe138c442e38706268ca0f39a3bcc069
Mayurs0 <mayurs@iconnectsolutions.com>
2024-12-22 07:49:33 +0530

    [LEARN]:Learned request handlers using rejex and routes order and all

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const app = express();
 //instance of express
 
 //request handler(function)
-app.use((req, res) => {
-  res.send("hello from server");
-}); //this will send hello from server on any request
+// app.use((req, res) => {
+// res.send("hello from server");
+//}); //this will send hello from server on any request
 //but in the app.use function we can give first args as route e.g /home then it will only send response on that route.
+app.get("/user/:id/:name/:pass", (req, res) => {
+  console.log(req?.params);
+  res.send({ firstName: "Mayur", lastName: "Shelke" });
+});
 app.use("/hello", (req, res) => {
   res.send("hello hello hello");
+});
+app.use("/test", (req, res) => {
+  res.send("test test test");
 });
 
 app.listen(3000, () => {
